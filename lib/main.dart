ade5102d3b08b777ce8d4898d3c2ff01ee7f6dbd
Osama Saad <osaad8031@gmail.com>
2024-08-27 16:34:33 +0300

    finish splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const News_App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class News_App extends StatelessWidget {
+  const News_App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routename: (context) => const HomeScreen(),
+      },
     );
   }
 }
